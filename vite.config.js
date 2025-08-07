291ba6e5855bca229a8e8fb49e7016c3175f5426
edrg2 <hibluesky1125@gmail.com>
2025-08-07 16:37:36 +0800

    CD

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,36 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, proccess.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+
+    // github 路徑設定
+    base: env.GITHUB_PAGES_DEPLOY === "ture" ? "/Login-form/" : "/",
+
+    // json-server
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:3001",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html"],
-      reportsDirectory: "./coverage",
+
+    // vitest
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.js",
+      coverage: {
+        provider: "v8",
+        reporter: ["text", "json", "html"],
+        reportsDirectory: "./coverage",
+      },
     },
-  },
+  };
 });
