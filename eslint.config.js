7a335ada173c570776c66357156b65727013447a
edrg2 <hibluesky1125@gmail.com>
2025-08-07 16:58:37 +0800

    debug lint v4

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,39 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  {
+    ignores: ["dist/**"],
+  },
   {
     files: ["**/*.{js,jsx}"],
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      react: react,
     },
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
       globals: {
         ...globals.browser,
-        ...globals.node,
         "import.meta": "readonly",
       },
       parserOptions: {
         ecmaFeatures: { jsx: true },
       },
     },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": "warn",
